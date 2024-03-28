c8285b5f9ba2803ca16ca3c810afbda5bccdc1c2
feidufuzhou <924262305@qq.com>
2024-03-28 09:41:21 +0800

    asd

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,6 +3,6 @@ import { Environment } from '../app/models/environment.model';
 export const environment: Environment = {
 
     // apiUrl: 'http://192.168.110.250:8080',apiUrl: 'http://888q277c78.zicp.fun:80',
-    apiUrl: 'http://888q277c78.zicp.fun:80'    ,
+    apiUrl: 'http://192.168.110.250:8080'    ,
 
 };
